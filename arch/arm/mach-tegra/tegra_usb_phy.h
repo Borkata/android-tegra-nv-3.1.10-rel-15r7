47568c9858fee8f3d4214cc1f6248716f8e3e275
Rohan Somvanshi <rsomvanshi@nvidia.com>
2012-07-04 10:43:10 +0000

    ARM: tegra: usb: turn off power during suspend
    Turn off regulators along with clocks during suspend
    
    Bug 991262
    
    Reviewed-on: http://git-master/r/108820
    (cherry picked from commit 41ae7ac21a2c08da0e4312b8395b380a704fa16c)
    
    Change-Id: Ib49eec5d91571d75881f4d252d956dece1d37d60
    Signed-off-by: Rakesh Bodla <rbodla@nvidia.com>
    Reviewed-on: http://git-master/r/105364
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User

--- arch/arm/mach-tegra/tegra_usb_phy.h
+++ arch/arm/mach-tegra/tegra_usb_phy.h
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-tegra/include/mach/tegra_usb_phy.h
  *
- * Copyright (c) 2011-2012, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
