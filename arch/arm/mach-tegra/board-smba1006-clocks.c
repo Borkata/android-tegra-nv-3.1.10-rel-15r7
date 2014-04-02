f661b5197cf44fe2257a2291ec376f2a597e91a2
Boris Popov <bvpopov@gmail.com>
2014-04-02 01:06:23 -0700

    board-smba1006-clocks: Update default clocks Update default clocks and add option for setting clocks so 48kHz audio is used.

--- arch/arm/mach-tegra/board-smba1006-clocks.c
+++ arch/arm/mach-tegra/board-smba1006-clocks.c
@@ -264,17 +264,44 @@ static __initdata struct tegra_clk_init_table smba_clk_init_table[] = {
 #endif
 
 static __initdata struct tegra_clk_init_table smba_clk_init_table[] = {
-	/* name			parent		 rate		enabled */
-	{ "cdev1",		NULL,		     0, 	true },
-	{ "blink",	"clk_32k",		 32768,		false},
-	{ "pll_p_out4",	"pll_p",  24000000,		true },
+	/* name			parent				rate	enabled */
+	{ "pll_m",		NULL,			0,		false},
+	/* 32khz system clock */
+	{ "clk_32k",		NULL,			32768,		true},		/* always on */
+	{ "rtc",		"clk_32k",			32768,		true},		/* rtc-tegra : must be always on */
+	{ "blink",		"clk_32k",			32768,		false},		/* used for bluetooth */
 	{ "pwm",	"clk_32k",		 32768,		false},
-	{ "uartc",		"pll_p",	 216000000, 	false},    /* tegra_uart.2 uart.0 */
-	{ "uartd",              "pll_p",         216000000,     false},    /* tegra_uart.3 uart.3 */
-	{ "i2s1",	"pll_a_out0",		 0,		false},
-	{ "i2s2",	"pll_a_out0",		 0,		false},
-	{ "spdif_out",	"pll_a_out0",	 0,		false},	
-	{ NULL,			NULL,		     0,			0},
+	/* Master clock */
+	{ "pll_p_out4",	"pll_p",  24000000,		true },
+	{ "sdmmc2",		"pll_p",		48000000,	false},		/* sdhci-tegra.1 */
+	{ "pwm",		"clk_m",		12000000,	true},		/* tegra-pwm.0 tegra-pwm.1 tegra-pwm.2 tegra-pwm.3*/
+	{ "cdev1",		NULL,			0,		true},
+	{ "uartc",		"pll_p",		216000000,	false},    /* tegra_uart.2 uart.0 */
+	{ "uartd",              "pll_p",                216000000,      false},    /* tegra_uart.3 uart.3 */
+	{ "csus",    "clk_m",          0,  false},    /* tegra_camera */
+	{ "isp",    "clk_m",    0,  false},    /* tegra_camera? */
+	{ "vi_sensor",    "pll_m",   20000000,  false},    /* tegra_camera */
+	{ "vi",      "pll_m",   21000000,  false},    /* tegra_camera */
+#ifdef SMBA1006_48KHZ_AUDIO
+	{ "pll_a",		"pll_p_out1",	 73728000,	true},		/* always on - audio clocks */
+	{ "pll_a_out0", 	"pll_a",	 12288000,	true},		/* always on - i2s audio */
+	{ "i2s1",    "pll_a_out0",    2822400,  false},    /* i2s.0 */
+	{ "i2s2",    "pll_a_out0",   12288000,  false},    /* i2s.1 */
+	{ "audio",		"pll_a_out0",	 0,	false},
+	{ "audio_2x",		"audio",	 0,	false},
+	{ "spdif_in",		"pll_p",	 36000000,	false},
+	{ "spdif_out",		"pll_a_out0",	  6144000,	false},
+#else
+	{ "pll_a",		"pll_p_out1",	 56448000,	true},		/* always on - audio clocks */
+	{ "pll_a_out0", 	"pll_a",	 11289600,	true},		/* always on - i2s audio */
+	{ "i2s1",    "pll_a_out0",    2822400,  false},    /* i2s.0 */
+	{ "i2s2",    "pll_a_out0",   11289600,  false},    /* i2s.1 */
+	{ "audio",		"pll_a_out0",	 0,	false},
+	{ "audio_2x",		"audio",	 0,	false},
+	{ "spdif_in",		"pll_p",	 36000000,	false},
+	{ "spdif_out",		"pll_a_out0",	  5644800,	false},
+#endif
+	{  NULL,		NULL,			0,		0},
 }; 
 
 void __init smba_clks_init(void)
